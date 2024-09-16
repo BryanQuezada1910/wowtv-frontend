9c5d052fe78ed125bb1266721a6a41c2a7bd0dba
Luis Alfaro <52721684+luisguztav20@users.noreply.github.com>
2024-09-16 10:26:31 -0600

    Reproductor en vivo del calnal

---
+++ src/components/Live.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+// This imports the functional component from the previous sample.
+import VideoJS from "./VideoJS";
+
+const Live = () => {
+  const playerRef = React.useRef(null);
+
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: "https://cdn.elsalvadordigital.com:1936/wowtv/smil:wowtv.smil/playlist.m3u8",
+        type: "application/x-mpegURL",
+      },
+    ],
+  };
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on("waiting", () => {
+      videojs.log("player is waiting");
+    });
+
+    player.on("dispose", () => {
+      videojs.log("player will dispose");
+    });
+  };
+
+  return (
+    <>
+      <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+    </>
+  );
+};
+
+export default Live;
